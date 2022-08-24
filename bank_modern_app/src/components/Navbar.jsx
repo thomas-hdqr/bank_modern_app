aa77d6ba4c157afb0759d054b219b900236fc0d9
Thomas <thomashaudiquer@gmail.com>
2022-08-24 15:56:21 +0100

    commit

---
+++ bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState} from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="Hoobank" className="w-[124px] h-[32px]"></img>
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] 
+              ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
+              text-white`}
+            >
+              <a href={`#${nav.id}`}>
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+          src={toggle ? close : menu }
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}></img>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
